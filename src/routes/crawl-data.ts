3dbb6fc37b0cedd49ee07d61af7208c8cbd2cac9
ngochiep30051998 <ngochiep30051998@gmail.com>
2020-10-07 00:29:32 +0700

    crawl data

--- src/routes/crawl-data.ts
+++ src/routes/crawl-data.ts
@@ -1,13 +1,18 @@
 import { Request, Response, Router } from 'express';
 import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
 import { ParamsDictionary } from 'express-serve-static-core';
-
+'use strict';
 import { paramMissingError } from '@shared/constants';
 
 import rp from 'request-promise';
 import cheerio from 'cheerio';
 import axios from 'axios';
 import * as admin from 'firebase-admin';
+
+// tslint:disable-next-line: no-var-requires
+const fs = require('fs');
+
+// tslint:disable-next-line: no-var-requires
 
 const router = Router();
 
@@ -27,6 +32,7 @@ router.get('/all', async (req: Request, res: Response) => {
         const $ = await rp(options);
         const productBox = $('.product-box');
         const productList = productBox.find('.product-box-list');
+        console.log(productBox)
         const data = [];
         // tslint:disable-next-line: prefer-for-of
         for (let i = 0; i < productList.length; i++) {
@@ -46,22 +52,22 @@ router.get('/all', async (req: Request, res: Response) => {
                 //     brand:  $(product).attr('data-brand'),
                 //     sku: $(product).attr('product-sku')
                 // }
-                const responseData = await axios.get(productUrl);
-                admin.database().ref('products').push({
-                    id: responseData.data.id,
-                    sku: responseData.data.sku,
-                    name: responseData.data.name,
-                    price: responseData.data.price,
-                    discount: responseData.data.discount,
-                    categories: responseData.data.categories,
-                    meta_title: responseData.data.meta_title,
-                    description: responseData.data.description,
-                    brand: responseData.data.brand,
-                    current_seller: responseData.data.current_seller,
-                    thumbnail_url: responseData.data.thumbnail_url,
+                // const responseData = await axios.get(productUrl);
+                // admin.database().ref('products').push({
+                //     id: responseData.data.id,
+                //     sku: responseData.data.sku,
+                //     name: responseData.data.name,
+                //     price: responseData.data.price,
+                //     discount: responseData.data.discount,
+                //     categories: responseData.data.categories,
+                //     meta_title: responseData.data.meta_title,
+                //     description: responseData.data.description,
+                //     brand: responseData.data.brand,
+                //     current_seller: responseData.data.current_seller,
+                //     thumbnail_url: responseData.data.thumbnail_url,
 
-                })
-                data.push(responseData.data);
+                // })
+                data.push(1);
             }
         }
         return res.status(OK).json({ data, total: data.length });
@@ -70,14 +76,37 @@ router.get('/all', async (req: Request, res: Response) => {
     }
 });
 
-router.get('/foody',async (req: Request, res: Response) =>{
-    const url = 'https://www.foody.vn/__get/AutoComplete/Keywords?provinceId=218&term=x%C3%B4i';
-    const params = {
-        restaurant_ids: [87201, 17036, 105260, 123400, 6451, 195959, 714204, 45097, 6452, 111511, 42603, 44367, 198641, 189507]
+router.get('/now', async (req: Request, res: Response) => {
+
+    const data = require('../assets/data/com.json');
+    const items = [];
+    for (const item of data) {
+        if (item.title.toLowerCase().includes('cơm')) {
+            item.price = item.price.replace(/[^\w\s]/gi, '');
+            item.promotionPrice = Number(item.price) - Math.ceil(Number(item.price) * 0.1);
+            admin.database().ref('categories/-MIyoqIy1Q3SlnmygjlN').push(item);
+            items.push(item);
+        }
     }
-    const responseData = await axios.get(url);
-    const productUrl = 'https://www.foody.vn';
-    // for(const item of responseData){}
-    return res.status(OK).json({data: responseData.data});
+    return res.json({ items });
+})
+
+router.get('/cat', async (req: Request, res: Response) => {
+    const cat = await admin.database().ref('categories').push({ categoryName: 'Trà sữa ' })
+
+    const data = require('../assets/data/trasua.json');
+    const items = [];
+    for (const item of data) {
+        if (item.title.toLowerCase().includes('trà')) {
+            item.price = item.price.replace(/[^\w\s]/gi, '');
+            item.promotionPrice = Number(item.price) - Math.ceil(Number(item.price) * 0.1);
+            admin.database().ref(`categories/${cat.key}/products`).push(item);
+            items.push(item);
+        }
+    }
+    return res.json({
+        key: cat.key,
+        data: items
+    })
 })
 export default router;
